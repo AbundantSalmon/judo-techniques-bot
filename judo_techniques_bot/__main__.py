47aa4eaf7973c88b26de6e22816dc808b201affb
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2021-09-05 00:34:04 +0800

    Create logging folder if does not already exists

--- judo_techniques_bot/__main__.py
+++ judo_techniques_bot/__main__.py
@@ -1,16 +1,23 @@
 import logging
+import os
 from datetime import datetime
 from pathlib import Path
 
 import main
 
 if __name__ == "__main__":
+    log_location = Path(__file__).parent / "logs/all.log"
+    try:
+        os.makedirs("/".join(str(log_location).split("/")[:-1]), exist_ok=False)
+    except OSError:
+        pass
+
     logging.Formatter.converter = lambda *args: datetime.utcnow().timetuple()
     logging.basicConfig(
         format="%(asctime)s\t%(levelname)s\t%(message)s",
         level=logging.INFO,
         handlers=[
-            logging.FileHandler(Path(__file__).parent / "logs/all.log"),
+            logging.FileHandler(log_location),
             logging.StreamHandler(),
         ],
     )
