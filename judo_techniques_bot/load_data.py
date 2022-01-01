5caf29e6047a7759905f44b313308e5367aa06f7
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2022-01-01 23:09:30 +0800

    Make `load_data.py` script useful

--- judo_techniques_bot/load_data.py
+++ judo_techniques_bot/load_data.py
@@ -1,6 +1,9 @@
 import csv
 import os
 
+from db import session_scope
+from models import Technique
+
 TECHNIQUES_DATA_FILE = "data/techniques_fixtures.csv"
 
 folder = os.path.dirname(os.path.abspath(__file__))
@@ -11,14 +14,24 @@ def retrieve_fixture_data():
     print("Retrieving technique data from:")
     print(file_location)
 
-    array_of_techniques = []
+    array_of_techniques: list[Technique] = []
+    with session_scope() as s:
+        with open(file_location, "r") as file:
+            for row in csv.DictReader(file):
+                array_of_techniques.append(
+                    Technique(
+                        japanese_display_name=row["japanese_display_name"],
+                        japanese_names=row["japanese_names"].split(","),
+                        english_names=row["english_names"].split(","),
+                        video_url=row["video_url"],
+                    )
+                )
+        s.bulk_save_objects(array_of_techniques)
 
-    with open(file_location, "r") as file:
-        for row in csv.DictReader(file):
-            japanese_names = row.pop("japanese_names")
-            row["japanese_names"] = japanese_names.split(",")
-            english_names = row.pop("english_names")
-            row["english_names"] = english_names.split(",")
-            array_of_techniques.append(row)
+    print(f"{len(array_of_techniques)} technique(s) data loaded!")
 
     return array_of_techniques
+
+
+if __name__ == "__main__":
+    retrieve_fixture_data()
